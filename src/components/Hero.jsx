4466b1e9c597e568c4b0e3eab48afb820745af93
masaki <j661masaki@gmail.com>
2022-10-11 22:05:31 +0900

    typing animation added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Typed from "react-typed";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import bgPic from "../assets/3d-mac.svg";
 
@@ -10,7 +11,12 @@ const Hero = () => {
         <div className="max-w-[900px] mx-auto px-8  flex flex-col fixed top-[25%] left-[20%] h-full z-10">
           <p className="text-xl text-white">Hi there, I'm</p>
           <h1 className="text-4xl sm:text-8xl font-bold pt-[10px] text-[#0fc2ba]">
-            Masaki Toyama
+            <Typed
+              strings={["Masaki Toyama"]}
+              typeSpeed={200}
+              // backSpeed={200}
+              // loop
+            />
           </h1>
           <p className="text-xl sm:text-5xl font-bold text-white pt-[10px]">
             I build things on the web
